b6a63d3d73aa5041264a845d8596986ef46597a2
Debayan Biswas <debayanbiswas1111@gmail.com>
2023-08-25 22:23:10 +0530

    added the cache part advanced

--- Web Application/cmd/web/main.go
+++ Web Application/cmd/web/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/pegasus7d/go-course/pkg/config"
 	"github.com/pegasus7d/go-course/pkg/handlers"
+	"github.com/pegasus7d/go-course/pkg/render"
 )
 const portnumber=":8080"
 
@@ -12,6 +15,12 @@ const portnumber=":8080"
 
 
 func main() {
+	var app config.AppConfig 
+	tc,err:=render.CreateTemplateCache()
+	if err!=nil{
+		log.Fatal("cannot create template cache")
+	}
+	app.TemplateCache=tc
 	http.HandleFunc("/",handlers.Home)
 	http.HandleFunc("/about",handlers.About)
 	fmt.Printf(fmt.Sprintf("Starting application on port %s",portnumber))
